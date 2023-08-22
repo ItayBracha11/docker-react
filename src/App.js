c73384443c70ed4e692a995b0404128d50019eb1
ItayBracha11 <itaybracha11@gmail.com>
2023-08-22 09:25:30 +0000

    check

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          shalom there!
+          shalom there!!
         </p>
         <a
           className="App-link"
